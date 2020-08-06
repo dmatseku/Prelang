0321cd0707570858799327fc95453286bfe1e6f6
dmatseku <ZNbig2A3>
2020-08-07 00:51:15 +0300

    added isset for variable

--- Prelang/Macros/InsertVariable.php
+++ Prelang/Macros/InsertVariable.php
@@ -21,9 +21,12 @@ class InsertVariable extends Macros
 
     public function finish(Fragment $fragment)
     {
-        $var = '';
-        if (!empty($fragment->match[3][0])) {
-            $var = '$this->'.trim($fragment->match[3][0], " \t\n\r\0\x0B'");
+        $var = trim($fragment->match[3][0], " \t\n\r\0\x0B'");
+        
+        if (!isset($this->$var)) {
+            $var = '';
+        } else {
+            $var = '$this->'.$var;
         }
 
         return $var;
