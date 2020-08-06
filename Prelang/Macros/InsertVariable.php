4461441d8c53b8b8ce868a0a3e47d6751a99fa29
dmatseku <ZNbig2A3>
2020-08-07 01:00:22 +0300

    removed isset))))

--- Prelang/Macros/InsertVariable.php
+++ Prelang/Macros/InsertVariable.php
@@ -22,14 +22,8 @@ class InsertVariable extends Macros
     public function finish(Fragment $fragment)
     {
         $var = trim($fragment->match[3][0], " \t\n\r\0\x0B'");
-        
-        if (!isset($this->$var)) {
-            $var = '';
-        } else {
-            $var = '$this->'.$var;
-        }
 
-        return $var;
+        return '$this->'.$var;
     }
 
     public function clean(Fragment $fragment): void {}
